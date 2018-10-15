4d1b7c5bea7226c05b13a7521ab3e695075b1e11
Garrison Grogan <garrison.grogan@columbia.edu>
2018-10-14 22:19:42 -0400

    pretty printing

--- ast.mli
+++ ast.mli
@@ -5,7 +5,10 @@
 
 *)
 
-type op = Add | Sub | Mul | Div | Mod | Eq | Union | Isec | Elof | Comp | Neq | And | Or
+type op = Add | Sub | Mul | Div | Mod | Eq
+          | Union | Isec | Elof | Comp | Neq 
+          | And | Or | LessEq | MoreEq
+          | More | Less | In
 type unop = Not (* cardinality is a delim like () *)
 type elmTypes = boolean | int | char | array| set (*more set types could be added here in future*)
 type datatype = setType of elmTypes | litType of elmTypes | arrType of elmTypes | void
@@ -45,22 +48,49 @@ type global = string * expr (* global assignments *)
 type program = global list * fdecl list (* a valid program is some globals and function declarations *)
 
 (* add pretty printing for the AST ie Add -> "+" *)
-let string_of_op = function
+let string_of_binop = function
     Add -> "+"
     | Sub -> "-"
     | Mul -> "*"
     | Div -> "/"
     | Mod -> "%"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-    | Sub -> "-"
-
-
+    | Eq  -> "="
+    | Neq -> "!="
+    | Less-> "<"
+    | More -> ">"
+    | LessEq-> "<="
+    | MoreEq-> ">="
+    | Union-> ":u"
+    | Isect -> ":n"
+    | Comp -> ":c"
+    | ElOf-> ":i"
+    | And -> "AND"
+    | Or -> "OR"
+    | In -> "in"
+let string_of_op = function
+    Not -> "!"
+let string_op_expr = function
+    IntLit(l)           -> string_of_int l
+    | charLit(l)        -> string_of_char l
+    | BoolLit(true)     -> "true"
+    | BoolList(false)   -> "false"
+    | Id(s)             -> s
+    | Binop(e1, o, e2)  -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+    | Unop(o, e)        -> string_of_uop o ^ sring_of_expr e
+    | fCall(f, e1)      -> f ^ "(" ^ String.concat", "(List.map string_of_expr e1)^ ")"
+    | Set(l)            -> "{" ^ String.concat " " (List.map string_of_expr l) ^ "}"
+    | Arr(l)            -> "[" ^ String.concat " " (List.map string_of_expr l) ^ "]"
+    | SetAccess(s,e)    -> s ^ "{" ^ string_of_expr e ^ "}"
+    | ArrAccess(a,e)    -> a ^ "[" ^ string_of_expr a^ "]"
+let rec string_of_stmt = function
+    Block(stmts) -> "Block{\n" ^ String.conocat "" (List.map string_of_stmt stmts) ^ "}\n"
+    | Expr(expr) -> string_of_expr ^ ";\n"
+    | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
+    | If(e, s) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+    | If(e, s1, s2) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_stmt s2 (*if else*)
+    | For(e1,e1,e3,s) -> "for(" ^ string_of_expr e1 ^ "; " ^ string_of_expr e2 ^ "; " ^ string_of_expr e3 ^ ")\n" ^ string_of_stmt s
+    | For(e1,e2,e3,s) -> "for(" ^ string_of_expr e1 ^ "; " ^ string_of_expr e2 ^ "; " ^ string_of_expr e3 ^ ")\n" ^ string_of_stmt s
+    | Foreach(e1,s1) -> "foreach(" ^ string_of_expr eq ^ ")\n" ^ string_of_stmt s1
+    | Assign(s, e) -> s ^ " = " string_of_expr ^ ";\n"
+    | SetElmAssign(s,e1,e2) -> s ^ "{" ^ string_of_expr e1 ^"} = " string_of_expr s2
+    | ArrayElmAssign(a,e1,e2) -> 
