0478835e5ac4bfe6df6d958b8ad9bf71f42b230f
Garrison Grogan <garrison.grogan@columbia.edu>
2018-10-14 20:58:03 -0400

    expr types, stmt types

--- ast.mli
+++ ast.mli
@@ -5,36 +5,29 @@
 
 *)
 
-type op = Add | Sub | Mul | Div | Mod | Eq | Union | Isec | Elof | Comp | Neq | SNeq
-type setTypes = int | set (*more set types could be added here in future*)
+type op = Add | Sub | Mul | Div | Mod | Eq | Union | Isec | Elof | Comp | Neq | And | Or
+type unop = Not (* cardinality is a delim like () *)
+type elmTypes = boolean | int | char | array| set (*more set types could be added here in future*)
+type datatype = setType of elmTypes | dataType of elmTypes | arrType of elmTypes
 
 
 type expr =
-Binop of expr * operator * expr
-| Lit of int
-| Seq of expr * expr
-| Asn of string * expr
-| Var of string
-| Set of setTypes
-| Union of set * u: *set
-| Isect of set * n: * set
-| Elof of setTypes * i: * set
-| Comp of set * c: * set
+Binop of expr * op * expr
+| intLit of int
+| charLit of char
+| boolLit of boolean
+| Set of elmTypes
+| Arr of elmTypes
+| Unop of unop * expr (* if we do string types or array slicing, their syntactic sugar needs to go here *)
 
-
-
-let string_of_op = function
-   Add    -> "+"
-  |Sub    -> "-"
-  |Mul    -> "*"
-  |Div    -> "/"
-  |Mod    -> "%"
-  |Eq     -> "==="
-  |Union  -> "u:"
-  |Isec   -> "n:"
-  |Elof   -> "i:"
-  |Comp   -> "c:"
-  |Neq    -> "!="
-  |SNeq   -> "!=="
-
-    
+and stmt =
+    Block of stmt
+    | Expr of expr
+    | If of expr * stmt * stmt
+    | For of 
+    | Foreach of
+    | Return of expr
+    | Break
+    | SetElementAssign of
+    | ArrayElementAssign of
+    | Assign of elmTypes * expr
