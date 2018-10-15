5207db17cc2d33983b614996170cce5f2a26907b
Garrison Grogan <garrison.grogan@columbia.edu>
2018-10-14 21:49:03 -0400

    fdecl done, doing pretty printing

--- ast.mli
+++ ast.mli
@@ -8,7 +8,7 @@
 type op = Add | Sub | Mul | Div | Mod | Eq | Union | Isec | Elof | Comp | Neq | And | Or
 type unop = Not (* cardinality is a delim like () *)
 type elmTypes = boolean | int | char | array| set (*more set types could be added here in future*)
-type datatype = setType of elmTypes | dataType of elmTypes | arrType of elmTypes
+type datatype = setType of elmTypes | litType of elmTypes | arrType of elmTypes | void
 
 
 type expr =
@@ -16,18 +16,51 @@ Binop of expr * op * expr
 | intLit of int
 | charLit of char
 | boolLit of boolean
-| Set of elmTypes
-| Arr of elmTypes
+| Set of elmTypes list
+| Arr of elmTypes list
+| SetAccess of string * expr
+| ArrayAccess of string * expr
+| fCall of string * expr list
 | Unop of unop * expr (* if we do string types or array slicing, their syntactic sugar needs to go here *)
 
 and stmt =
-    Block of stmt
+    Block of stmt list 
     | Expr of expr
     | If of expr * stmt * stmt
-    | For of expr * stmt 
+    | For of expr * expr * expr * stmt 
     | Foreach of expr * stmt
     | Return of expr
     | Break
-    | SetElementAssign of stmt
-    | ArrayElementAssign of stmt
-    | Assign of elmTypes * expr
+    | SetElementAssign of string * expr * expr
+    | ArrayElementAssign of string * expr * expr
+    | Assign of string * expr
+
+type fdecl = { (* function declaration *)
+    ftype : datatype
+    fname : string
+    parameters: string list
+    body : stmt list
+}
+type global = string * expr (* global assignments *)
+type program = global list * fdecl list (* a valid program is some globals and function declarations *)
+
+(* add pretty printing for the AST ie Add -> "+" *)
+let string_of_op = function
+    Add -> "+"
+    | Sub -> "-"
+    | Mul -> "*"
+    | Div -> "/"
+    | Mod -> "%"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+    | Sub -> "-"
+
+
