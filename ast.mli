0d9a9c8788fcb9cde44c695411b42079b7883a30
Ryan Chun <ryan.chun@columbiaspectator.com>
2018-10-22 17:27:25 -0400

    Parser Edit / AST Edit / More formatting

--- ast.mli
+++ ast.mli
@@ -33,7 +33,8 @@ and stmt = Block                of stmt list
 type fdecl = { (* function declaration *)
                 ftype : datatype;
                 fname : string;
-                parameters: string list; (* !!IMPORTANT: Shouldn't it have bindings? *)
+                parameters: string list; (* !!IMPORTANT: Shouldn't it be a list of bindings *)
+                (* !!IMPORTANT: Maybe we should add bindings here *)
                 body : stmt list;
             }
 
@@ -42,59 +43,62 @@ type program = global list * fdecl list (* a valid program is some globals and f
 
 (* add pretty printing for the AST ie Add -> "+" *)
 let string_of_binop = function
-    Add -> "+"
-    | Sub -> "-"
-    | Mul -> "*"
-    | Div -> "/"
-    | Mod -> "%"
-    | Eq  -> "="
-    | Neq -> "!="
-    | Less-> "<"
-    | More -> ">"
-    | LessEq-> "<="
-    | MoreEq-> ">="
-    | Union-> ":u"
-    | Isect -> ":n"
-    | Comp -> ":c"
-    | ElOf-> ":i"
-    | And -> "AND"
-    | Or -> "OR"
-    | In -> "in"
+    Add         -> "+"
+    | Sub       -> "-"
+    | Mul       -> "*"
+    | Div       -> "/"
+    | Mod       -> "%"
+    | Eq        -> "="
+    | Neq       -> "!="
+    | Less      -> "<"
+    | More      -> ">"
+    | LessEq    -> "<="
+    | MoreEq    -> ">="
+    | Union     -> ":u"
+    | Isect     -> ":n"
+    | Comp      -> ":c"
+    | ElOf      -> ":i"
+    | And       -> "AND"
+    | Or        -> "OR"
+    | In        -> "in"
+
 let string_of_unop = function
     Not -> "!"
+
 let string_binop_expr = function
-    IntLit(l)           -> string_of_int l
-    | charLit(l)        -> string_of_char l
-    | BoolLit(true)     -> "true"
-    | BoolLit(false)   -> "false"
-    | Id(s)             -> s (* !!IMPORTANT: ID is not defined *)
-    | Binop(e1, o, e2)  -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-    | Unop(o, e)        -> string_of_unop o ^ sring_of_expr e
-    | Call(f, e1)      -> f ^ "(" ^ String.concat", "(List.map string_of_expr e1)^ ")"
-    | Set(l)            -> "{" ^ String.concat " " (List.map string_of_expr l) ^ "}"
-    | Arr(l)            -> "[" ^ String.concat " " (List.map string_of_expr l) ^ "]"
-    | SetAccess(s,e)    -> s ^ "{" ^ string_of_expr e ^ "}"
-    | ArrAccess(a,e)    -> a ^ "[" ^ string_of_expr a^ "]"
+    IntLit(l)               -> string_of_int l
+    | charLit(l)            -> string_of_char l
+    | BoolLit(true)         -> "true"
+    | BoolLit(false)        -> "false"
+    | Id(s)                 -> s (* !!IMPORTANT: ID is not defined *)
+    | Binop(e1, o, e2)      -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+    | Unop(o, e)            -> string_of_unop o ^ sring_of_expr e
+    | Call(f, e1)           -> f ^ "(" ^ String.concat", "(List.map string_of_expr e1)^ ")"
+    | Set(l)                -> "{" ^ String.concat " " (List.map string_of_expr l) ^ "}"
+    | Arr(l)                -> "[" ^ String.concat " " (List.map string_of_expr l) ^ "]"
+    | SetAccess(s,e)        -> s ^ "{" ^ string_of_expr e ^ "}"
+    | ArrAccess(a,e)        -> a ^ "[" ^ string_of_expr a^ "]"
 
 let rec string_of_stmt = function
-    Block(stmts)        -> "Block{\n" ^ String.conocat "" (List.map string_of_stmt stmts) ^ "}\n"
-    | Expr(expr)        -> string_of_expr ^ ";\n"
-    | Return(expr)      -> "return " ^ string_of_expr expr ^ ";\n"
-    | If(e, s)          -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-    | If(e, s1, s2)     -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_stmt s2 (*if else*)
-    | For(e1,e2,e3,s)   -> "for(" ^ string_of_expr e1 ^ "; " ^ string_of_expr e2 ^ "; " ^ string_of_expr e3 ^ ")\n" ^ string_of_stmt s
-    | Foreach(e1,s1)    -> "foreach(" ^ string_of_expr eq ^ ")\n" ^ string_of_stmt s1
-    | Assign(s, e)      -> s ^ " = " string_of_expr ^ ";\n"
-    | SetElmAssign(s,e1,e2)   -> s ^ "{" ^ string_of_expr e1 ^"} = " string_of_expr e2 ^";\n"
-    | ArrayElmAssign(a,e1,e2) -> a ^"[" ^ string_of_expr e1 ^"] = " string_of_expr e2 ^";\n"
-    | Break -> "break;\n"
+    Block(stmts)                -> "Block{\n" ^ String.conocat "" (List.map string_of_stmt stmts) ^ "}\n"
+    | Expr(expr)                -> string_of_expr ^ ";\n"
+    | Return(expr)              -> "return " ^ string_of_expr expr ^ ";\n"
+    | If(e, s)                  -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+    | If(e, s1, s2)             -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_stmt s2 (*if else*)
+    | For(e1,e2,e3,s)           -> "for(" ^ string_of_expr e1 ^ "; " ^ string_of_expr e2 ^ "; " ^ string_of_expr e3 ^ ")\n" ^ string_of_stmt s
+    | Foreach(e1,s1)            -> "foreach(" ^ string_of_expr eq ^ ")\n" ^ string_of_stmt s1
+    | Assign(s, e)              -> s ^ " = " string_of_expr ^ ";\n"
+    | SetElmAssign(s,e1,e2)     -> s ^ "{" ^ string_of_expr e1 ^"} = " string_of_expr e2 ^";\n"
+    | ArrayElmAssign(a,e1,e2)   -> a ^"[" ^ string_of_expr e1 ^"] = " string_of_expr e2 ^";\n"
+    | Break                     -> "break;\n"
+
 let rec string_of_typ = function
-    datatype(int)    -> "int"
-    | datatype(char) -> "char"
-    | datatype(bool) -> "boolean"
-    | datatype(void) -> "void"
-    | SetType(s)     -> string_of_typ (datatype(s))
-    | ArrType(a)     -> string_of_typ (dataypte(a))
+    datatype(int)           -> "int"
+    | datatype(char)        -> "char"
+    | datatype(bool)        -> "boolean"
+    | datatype(void)        -> "void"
+    | SetType(s)            -> string_of_typ (datatype(s))
+    | ArrType(a)            -> string_of_typ (dataypte(a))
 
 let string_of_init (s, e) = s ^ " = " ^ string_of_expr e ^ ";\n"
 
