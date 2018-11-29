ac4507fa72d361b194b13f6ff55a26a5cb0daab0
CodeKoning <rjk2153@P50s-1.allurehome.com>
2018-11-28 20:01:35 -0500

    Changed printbig to accept chars in semant and codegen.

--- semant.ml
+++ semant.ml
@@ -41,7 +41,7 @@ let check (globals, functions) =
     in List.fold_left add_bind StringMap.empty [ ("print", Int);
 			                         ("printb", Boolean);
 			                         ("printf", String);
-			                         ("printbig", Int) ]
+			                         ("printbig", Char) ]
   in
 
   (* Add function name to symbol table *)
