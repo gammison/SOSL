83309b5e9896eecdb83e909dc947c9c2c1a91145
CodeKoning <rjk2153@P50s-1.allurehome.com>
2018-12-01 23:40:29 -0500

    fixing error from last merge

--- codegen.ml
+++ codegen.ml
@@ -166,8 +166,6 @@ let translate (globals, functions) =
       | SCall ("printf", [(_, e)]) -> 
 	  L.build_call printf_func [| float_format_str ; (expr builder e) |]
 	    "printf" builder
-      | SCall ("print_string", [(_, e)]) -> 
-	  L.build_call print_string_func [| (expr builder e) |] "print_string" builder
       | SCall (f, args) ->
          let (fdef, fdecl) = StringMap.find f function_decls in
 	 let llargs = List.rev (List.map (expr builder) (List.rev_map snd args)) in
